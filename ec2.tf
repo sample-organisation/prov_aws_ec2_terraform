b96739bcea55d4c910e40d7e8774791b6e9b2c3a
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-07-17 17:48:00 +0530

    adds ssh key

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,12 @@ provider "aws" {
   region      = "${var.vpc_region}"
 }
 
+# SSH key pair for accessing ec2 machine
+resource "aws_key_pair" "sshKey" {
+  key_name   = "${var.aws_key_name}"
+  public_key = "${var.public_ssh_key}"
+}
+
 # instances
 resource "aws_instance" "ec2Instances" {
   count = 1
